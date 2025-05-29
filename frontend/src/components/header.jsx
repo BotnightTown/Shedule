41c9e0659f7ef90407269a480dff470c28b6f20c
BotnightTown <minecraftv871@gmail.com>
2025-05-29 11:08:01 +0300

    small fix

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 
 function Header({ onSidebarToggle }){
   return(
-    <header className="h-max flex flex-row justify-between items-center p-4 text-2xl lg:text-3xl">
+    <header className="h-max flex flex-row justify-start items-center p-4 text-2xl lg:text-3xl">
       <TfiAlignLeft className="w-10 mr-10 cursor-pointer" onClick={onSidebarToggle}/>
-      <NavLink className="w-full text-left font-medium" to="/">Schedule</NavLink>
+      <NavLink className="w-max text-left font-medium" to="/">Schedule</NavLink>
       {/* <MdMoreHoriz className="w-10"/> */}
     </header>
   )
